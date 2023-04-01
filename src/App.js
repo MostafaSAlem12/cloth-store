e0d2c80098c904f69606bee5529b254cfedf905c
Mostafa Salem Ali <86600378+MostafaSAlem12@users.noreply.github.com>
2023-04-01 17:18:06 +0200

    adding firebase and sign in in nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route, Outlet } from 'react-router-dom';
 import Home from "./routes/home/home.component";
 import Navigation from './routes/navigation/navigation.component';
-
+import SignIn from './routes/sign-in/sign-in.component.jsx'
 
 const Shop = () => {
   return <h1>shop !!</h1>
@@ -13,6 +13,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={< Home />} />
         <Route path='/shop' element={< Shop />} />
+        <Route path='/sign-in' element={< SignIn />} />
       </Route>
 
     </Routes>
